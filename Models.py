5a21c405a310c07cf1f4cd3c9721b4f2938a56d8
Johneinsteinwong <johneinsteinwong@gmail.com>
2024-10-23 23:41:11 +0800

    added weighted loss

--- Models.py
+++ Models.py
@@ -5,23 +5,25 @@ from sklearn.exceptions import NotFittedError
 
 class LatentVariableModel():
     
-    def __init__(self, dist, l1, l2):
+    def __init__(self, dist, l1, l2, w0, w1):
         if l1<0 or l2<0:
             raise ValueError('l1 and l2 must be non-negative!')
         self.param = None
         self.dist = dist
         self.l1 = l1
         self.l2 = l2
+        self.w0 = w0
+        self.w1 = w1
 
     def __loss(self, param, x, y):
         p = param[:-1]
         b = param[-1]
         score = np.dot(x,p) + b
-        diff = -y*self.dist.logcdf(score, loc=0, scale=1) - (1-y)*self.dist.logcdf(-score, loc=0, scale=1)
+        diff = -self.w1*y*self.dist.logcdf(score, loc=0, scale=1) - self.w0*(1-y)*self.dist.logcdf(-score, loc=0, scale=1)
         obj = np.mean(diff) + self.l1*np.linalg.norm(param, ord=1) + self.l2*np.linalg.norm(param, ord=2)
         return obj
 
-    def fit(self, x, y):
+    def fit(self, x, y, tol=1e-5, maxiter=1000):
         loss_values = []
         self.param = np.zeros(x.shape[1]+1)
         def callback(param):
@@ -29,11 +31,11 @@ class LatentVariableModel():
             loss_values.append(current_loss)
             print("Current loss:", current_loss)
         
-        result = minimize(self.__loss, self.param, args=(x, y), method='BFGS', tol=1e-3, options={'maxiter':1000}, callback=callback)
+        result = minimize(self.__loss, self.param, args=(x, y), method='BFGS', tol=tol, options={'maxiter':maxiter}, callback=callback)
         self.param = result.x
         return result, loss_values
     
-    def predict(self, x):
+    def predict_score(self, x):
         if self.param is None:
             raise NotFittedError("Model is not fitted yet. Call 'fit' with appropriate arguments before using this estimator.")
         p = self.param[:-1]
@@ -42,13 +44,17 @@ class LatentVariableModel():
         return score
     
     def predict_proba(self, x):
-        score = self.predict(x)
+        score = self.predict_score(x)
         return self.dist.cdf(score, loc=0, scale=1)
+
+    def predict(self, x, thr=0.5):
+        prob = self.predict_proba(x)
+        return (prob>=thr).astype(int)
     
 class logisticModel(LatentVariableModel):
-    def __init__(self, l1=0.0, l2=0.0):
-        super().__init__(logistic, l1, l2)
+    def __init__(self, l1=0.0, l2=0.0, w0=1, w1=1):
+        super().__init__(logistic, l1, l2, w0, w1)
 
 class probitModel(LatentVariableModel):
-    def __init__(self, l1=0.0, l2=0.0):
-        super().__init__(norm, l1, l2)
+    def __init__(self, l1=0.0, l2=0.0, w0=1, w1=1):
+        super().__init__(norm, l1, l2, w0, w1)
---
+++ util.py
@@ -0,0 +1,67 @@
+from sklearn.metrics import confusion_matrix, accuracy_score, \
+precision_score, recall_score, f1_score, roc_curve, roc_auc_score
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+def print_confusion_matrix(confusion_matrix, class_names, figsize = (6,5)):
+  df_cm = pd.DataFrame(
+      confusion_matrix, index=class_names, columns=class_names,
+  )
+  fig = plt.figure(figsize=figsize)
+  try:
+      heatmap = sns.heatmap(df_cm, annot=True, fmt="d", cmap='Reds')
+  except ValueError:
+      raise ValueError("Confusion matrix values must be integers.")
+  heatmap.yaxis.set_ticklabels(heatmap.yaxis.get_ticklabels(), rotation=0, ha='right', fontsize=20)
+  heatmap.xaxis.set_ticklabels(heatmap.xaxis.get_ticklabels(), rotation=0, ha='right', fontsize=20)
+  plt.title('Confusion matrix', fontsize=25)
+  plt.ylabel('True label', fontsize=17)
+  plt.xlabel('Predicted label', fontsize=17)
+  return fig
+
+
+def evaluate_model(y_true, y_pred, y_pred_prob):
+  '''
+  y_true: a numpy array of true class label, containing 0 and 1
+  y_pred: a numpy array of predicted class label, containing 0 and 1
+  y_pred_prob: a numpy array of predicted probability of belonging to a class
+  '''
+
+  tn, fp, fn, tp = confusion_matrix(y_true, y_pred).ravel()
+  cm = np.array([tp,fn,fp,tn]).reshape(2,2)
+  acc = accuracy_score(y_true, y_pred)
+  precision = precision_score(y_true, y_pred)
+  recall = recall_score(y_true, y_pred)
+  f1 = f1_score(y_true, y_pred)
+  roc_auc = roc_auc_score(y_true, y_pred_prob)
+
+  print('Accuracy: %.3f'%acc)
+  print('Precision: %.3f'%precision)
+  print('Recall: %.3f'%recall)
+  print('F1 Score: %.3f'%f1)
+  print('AUC score: %.3f \n'%roc_auc)
+  print_confusion_matrix(cm,[1,0])
+
+def plot_roc(y_true, y_pred_prob):
+  '''
+  y_true: a numpy array of true class label, containing 0 and 1
+  y_pred_prob: a numpy array of predicted probability of belonging to a class
+  '''
+
+  fpr, tpr, thresholds = roc_curve(y_true, y_pred_prob, pos_label=None, drop_intermediate=False)
+  roc_auc = roc_auc_score(y_true, y_pred_prob)
+  plt.figure()
+  lw = 2
+  plt.plot(fpr, tpr, color='darkorange',
+          lw=lw, label='ROC curve (area = %0.3f)' % roc_auc)
+  plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+  plt.xlim([0.0, 1.0])
+  plt.ylim([0.0, 1.05])
+  plt.xlabel('False Positive Rate')
+  plt.ylabel('True Positive Rate')
+  plt.title('ROC Curve')
+  plt.legend(loc="lower right")
+  plt.show()
+
