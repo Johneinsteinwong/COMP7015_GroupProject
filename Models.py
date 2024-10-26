f55060a91b12a80db1a3c037cb0ec166ad83c140
Johneinsteinwong <johneinsteinwong@gmail.com>
2024-10-26 20:11:00 +0800

    added Wald test

--- Models.py
+++ Models.py
@@ -1,8 +1,11 @@
 import numpy as np
-from scipy.stats import norm, logistic
+from scipy.stats import norm, logistic, chi2
 from scipy.optimize import minimize
 from sklearn.exceptions import NotFittedError
 from sklearn.base import BaseEstimator, ClassifierMixin
+from scipy.optimize import approx_fprime
+import pandas as pd
+from tabulate import tabulate
 
 class LatentVariableModel(BaseEstimator):
     
@@ -13,6 +16,29 @@ class LatentVariableModel(BaseEstimator):
         self.l2 = l2
         self.w = w
 
+    def __calculate_hessian(self, func, param, args, epsilon=1e-5):
+        n = len(param)
+        hessian_matrix = np.zeros((n, n))
+        
+        # Calculate the gradient at params
+        gradient = approx_fprime(param, func, epsilon, *args)
+        
+        # Approximate Hessian
+        for i in range(n):
+            params_eps = np.copy(param)
+            params_eps[i] += epsilon
+            gradient_eps = approx_fprime(params_eps, func, epsilon, *args)
+            hessian_matrix[:, i] = (gradient_eps - gradient) / epsilon
+        
+        return hessian_matrix
+
+    def __log_likelihood(self, param, x, y):
+        p = param[:-1]
+        b = param[-1]
+        score = np.dot(x, p) + b
+        # The original (unweighted) log-likelihood
+        return np.sum( -y*self.dist.logcdf(score, loc=0, scale=1) - (1-y)*self.dist.logcdf(-score, loc=0, scale=1) )
+
     def __loss(self, param, x, y):
         p = param[:-1]
         b = param[-1]
@@ -20,6 +46,7 @@ class LatentVariableModel(BaseEstimator):
         diff = -2*self.w*y*self.dist.logcdf(score, loc=0, scale=1) - 2*(1-self.w)*(1-y)*self.dist.logcdf(-score, loc=0, scale=1)
         obj = np.mean(diff) + self.l1*np.linalg.norm(param, ord=1) + self.l2*np.linalg.norm(param, ord=2)
         return obj
+
 
     def fit(self, x, y, tol=1e-5, maxiter=1000, verbose=False):
         if self.l1<0 or self.l2<0:
@@ -38,7 +65,38 @@ class LatentVariableModel(BaseEstimator):
         
         result = minimize(self.__loss, self.param, args=(x, y), method='BFGS', tol=tol, options={'maxiter':maxiter}, callback=callback)
         self.param = result.x
-        return result, loss_values
+
+        # Wald test
+        hessian_matrix = self.__calculate_hessian(self.__log_likelihood, self.param, args=(x, y))
+        self.hessian_inv = np.linalg.inv(hessian_matrix)
+
+        se, wald_stats, p_values = self.wald_test()
+
+        wald_df = pd.DataFrame(
+                zip(
+                    self.param,
+                    se.round(4),
+                    wald_stats.round(4),
+                    p_values.round(4)
+                ), columns=['coef','std err','z','P>|z|']
+        ).sort_values(by='P>|z|', ascending=True).reset_index(inplace=False)
+
+        if verbose:
+            print('Wald test summary:')
+            print(tabulate(wald_df, headers='keys', tablefmt='psql'))
+
+        res_dict = {
+            'result': result,
+            'loss': loss_values,
+            'wald_result': wald_df
+        }
+        return res_dict
+
+    def wald_test(self):
+        se = np.sqrt(np.diag(self.hessian_inv))
+        wald_stats = self.param / se
+        p_values = 2*(1 - norm.cdf(np.abs(wald_stats)))
+        return se, wald_stats, p_values
     
     def predict_score(self, x):
         if self.param is None:
